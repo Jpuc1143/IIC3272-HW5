6c6892fa1edbb974745fc6a38b813f2b9fd2e58d
jpuc1143 <jaime.perez@uc.cl>
2023-12-14 02:32:02 -0300

    Removed sample code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,10 @@
 import { EthProvider } from "./contexts/EthContext";
-import Intro from "./components/Intro/";
-import Setup from "./components/Setup";
-import Demo from "./components/Demo";
-import Footer from "./components/Footer";
 import Game from "./roulette/game";
 
 function App() {
   return (
     <EthProvider>
-      <div id="App">
-        <div className="container">
-          <Intro />
-          <hr />
-          <Setup />
-          <hr />
-          <Demo />
-          <hr />
-          <Footer />
-        </div>
-      </div>
-      <div className="game-container">
+            <div className="game-container">
         <Game />
       </div>
     </EthProvider>
